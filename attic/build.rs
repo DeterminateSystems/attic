40a1ba601de9a27f7222a5929a395ad6825cf1eb
Emily <hello@emily.moe>
2025-02-20 18:16:48 +0000

    attic/nix_store: update bindings build for Nix 2.26
    `#include <nix/…>` is broken by the componentized Meson build. This
    is a regression, but the `.pc` files add the `include/nix` directory
    of each library to the include path directly, so we can work around
    it by dropping the prefixes and using `system-deps` to simplify the
    native dependency handling.
    
    I’ve discussed this with Nix upstream, who plan on fixing it;
    the `.hpp` part of this commit should be reverted after it is fixed
    upstream, as the `.pc` files will no longer pollute the namespace in
    this manner and the previous form of the `#include`s will need to
    be restored. Apparently the `-include nix/config-*.hh` pattern may
    change as well.

--- attic/build.rs
+++ attic/build.rs
@@ -9,30 +9,17 @@ fn main() {
 
 #[cfg(feature = "nix_store")]
 fn build_bridge() {
-    let nix_include_dir = &pkg_config::probe_library("nix-store")
-        .unwrap()
-        .include_paths[0];
-
-    cxx_build::bridge("src/nix_store/bindings/mod.rs")
-        .file("src/nix_store/bindings/nix.cpp")
-        .flag("-std=c++2a")
-        .flag("-O2")
-        .flag("-include")
-        .flag("nix/config.h")
-        .flag("-I")
-        .flag(&*nix_include_dir.to_string_lossy())
-        .compile("nixbinding");
+    let deps = system_deps::Config::new().probe().unwrap();
 
     println!("cargo:rerun-if-changed=src/nix_store/bindings");
 
-    // the -l flags must be after -lnixbinding
-    pkg_config::Config::new()
-        .atleast_version("2.4")
-        .probe("nix-store")
-        .unwrap();
-
-    pkg_config::Config::new()
-        .atleast_version("2.4")
-        .probe("nix-main")
-        .unwrap();
+    cxx_build::bridge("src/nix_store/bindings/mod.rs")
+        .file("src/nix_store/bindings/nix.cpp")
+        .std("c++2a")
+        .includes(deps.all_include_paths())
+        .flag("-include")
+        .flag("config-store.hh")
+        .flag("-include")
+        .flag("config-main.hh")
+        .compile("nixbinding");
 }
